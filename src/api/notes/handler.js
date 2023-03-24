04e299d41d6829fa234c50b904f20280e00f8734
ilham arsy <ilhamarsy16@gmail.com>
2023-03-24 11:54:48 +0800

    initial commit

---
+++ src/api/notes/handler.js
@@ -0,0 +1,72 @@
+const autoBind = require('auto-bind');
+
+class NotesHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postNoteHandler(request, h) {
+    this._validator.validateNotePayload(request.payload);
+    const { title = 'untitled', body, tags } = request.payload;
+
+    const noteId = await this._service.addNote({ title, body, tags });
+
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        noteId,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  async getNotesHandler() {
+    const notes = await this._service.getNotes();
+    return {
+      status: 'success',
+      data: {
+        notes,
+      },
+    };
+  }
+
+  async getNoteByIdHandler(request) {
+    const { id } = request.params;
+    const note = await this._service.getNoteById(id);
+    return {
+      status: 'success',
+      data: {
+        note,
+      },
+    };
+  }
+
+  async putNoteByIdHandler(request) {
+    this._validator.validateNotePayload(request.payload);
+    const { id } = request.params;
+
+    await this._service.editNoteById(id, request.payload);
+
+    return {
+      status: 'success',
+      message: 'Catatan berhasil diperbarui',
+    };
+  }
+
+  async deleteNoteByIdHandler(request) {
+    const { id } = request.params;
+    await this._service.deleteNoteById(id);
+
+    return {
+      status: 'success',
+      message: 'Catatan berhasil dihapus',
+    };
+  }
+}
+
+module.exports = NotesHandler;
---
+++ src/server.js
@@ -0,0 +1,61 @@
+// mengimpor dotenv dan menjalankan konfigurasinya
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const notes = require('./api/notes');
+const ClientError = require('./exceptions/ClientError');
+const NotesService = require('./services/postgres/NotesService');
+const NotesValidator = require('./validator/notes');
+
+const init = async () => {
+  const notesService = new NotesService();
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+    if (response instanceof Error) {
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
+
+      if (!response.isServer) {
+        return h.continue;
+      }
+
+      const newResponse = h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
+      });
+      newResponse.code(500);
+      return newResponse;
+    }
+
+    return h.continue;
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
